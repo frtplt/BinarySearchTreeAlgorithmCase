b8326626f5b54b9278c7e490c5226332bc5d9228
frtplt <firat_p@hotmail.com>
2022-01-23 13:19:50 +0300

    BinarySearchTreeAlgorithm

---
+++ BinarySearchTreeCase.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let dizi = [7, 5, 1, 8, 3, 6, 0, 9, 4, 2]
+
+/*
+ 
+1.Aşama =                    5
+2.Aşama =         2                        7
+3.Aşama =     1       4              6          9
+4.Aşama =   0       3                         8
+          
+*/
